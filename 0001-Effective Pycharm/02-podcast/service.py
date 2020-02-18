a4c864e42da8e47514a05ed357cc58f6f8cc4281
TheRedSage <sapphirelife@gmail.com>
2020-02-17 22:47:20 -0600

    Ch 4 done.

---
+++ 0001-Effective Pycharm/02-podcast/service.py
@@ -0,0 +1,31 @@
+import requests
+from collections import namedtuple
+from xml.etree import ElementTree
+
+Episode = namedtuple("Episode", "title link pubdate show_id")
+episode_data = {}
+
+
+def download_info():
+    url = 'https://talkpython.fm/episodes/rss'
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    dom = ElementTree.fromstring(resp.text)
+
+    items = dom.findall("channel/item")
+    episode_count = len(items)
+
+    for idx, item in enumerate(items):
+        episode = Episode(
+            item.find("title").text,
+            item.find("link").text,
+            item.find("pubdate"),  # had to remove .text from this because of data type
+            episode_count - idx - 1
+        )
+
+        episode_data[episode.show_id] = episode
+
+
+def get_episode(show_id: int) -> Episode:
+    return episode_data.get(show_id)
