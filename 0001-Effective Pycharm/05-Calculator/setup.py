4568eb6df647ee79f048545e5813f23f1f9455dd
TheRedSage <sapphirelife@gmail.com>
2020-03-13 22:23:31 -0500

    CH 11 Done

---
+++ 0001-Effective Pycharm/05-Calculator/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Calculator',
+    version='0.1.0',
+    packages=['calculator'],
+    url='https://calculat.or',
+    license='MIT',
+    author='The Red Sage',
+    author_email='Red@theredsage.com',
+    description='A Pythonic Calculator'
+)
